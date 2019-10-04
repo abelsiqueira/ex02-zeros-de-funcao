361800b67a1232973a0da380ed8dfb607cf1c26b
Lucas Akio <44353304+Xedoux@users.noreply.github.com>
2019-10-04 15:17:03 -0300

    Update problemas.jl
    Dois últimos

--- problemas.jl
+++ problemas.jl
@@ -1,6 +1,5 @@
 const ProblemList =
 [
-  EquacaoNL(x -> -x*exp((sin(x)) + x*log(exp(x)) + x*cos(x)), 3.0,"soma tudo"),
   EquacaoNL(x -> 1/(atan(x^2+1) + log(1/exp(x))), 0.0,"soma com inversas"),
   EquacaoNL(x -> exp(x)*sin(x + π/2), 0.0,"Sen vezes exp")
 ]
