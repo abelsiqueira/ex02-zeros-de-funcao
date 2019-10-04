8301a0b0f5d8e593fb95f6b7db30d9e6db612b18
Lucas Akio <44353304+Xedoux@users.noreply.github.com>
2019-10-04 14:13:35 -0300

    Update problemas.jl

--- problemas.jl
+++ problemas.jl
@@ -11,5 +11,8 @@ const ProblemList =
   EquacaoNL(x -> sin(x) + max(3 - x, 0)^2 + max(x - 3.2, 0)^2, 1.0, "π por sin"),
   EquacaoNL(x -> 1 / (1 + exp(-0.1x + 1)) - 0.5, 0.0, "Sigmoid"),
   EquacaoNL(x -> x^6 - 48*x^4 + 1344*x^2 - 4096, 0.0, "Grau 6"),
-  EquacaoNL(x -> sin(exp(x))+cos(exp(x)), 0.0, "sen + cos de exp")
+  EquacaoNL(x -> sin(exp(x))+cos(exp(x)), 0.0, "sen + cos de exp"),
+  EquacaoNL(x -> log(x^2 + 5)/exp(sin(x))+x, 0.0,"Log exp e sen"),
+  EquacaoNL(x -> log(sin(x)^2), 0.0,"Log e sin"),
+  EquacaoNL(x -> sin(x - ℯ)/exp(x), 0.0,"Sen por exp")
 ]
