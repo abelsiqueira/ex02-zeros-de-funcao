5ab77cb041547477316f2444ad974dece4b293c8
Lucas Akio <44353304+Xedoux@users.noreply.github.com>
2019-10-06 19:09:22 -0300

    Update problemas.jl

--- problemas.jl
+++ problemas.jl
@@ -1,5 +1,4 @@
 const ProblemList =
 [
-  EquacaoNL(x -> x/(log(x^2 + sin(x) + 2) + 5/4, 3.0,"x por ln composta"),
-  EquacaoNL(x -> x*exp((sin(x)) + x*sin(x) + 1), 3.0,"soma tudo")
+  EquacaoNL(x -> x*exp((sin(x)) + x*sin(x) + 1), 3.0, "soma tudo")
 ]
